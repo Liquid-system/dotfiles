45e01553c5cc6190cadeccacbc37dcc6a0047ee6
Liquid-system <80927666+Liquid-system@users.noreply.github.com>
2022-12-25 17:01:30 +0900

    fix: deleted file

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,7 +3,6 @@ vim.g.loaded_perl_provider = 0
 
 require "core.config"
 require "core.autocmd"
-require "core.disable"
 require "core.options"
 
 -- leaderキーをspaceに変更
@@ -21,6 +20,30 @@ if not vim.loop.fs_stat(lazypath) then
 end
 
 vim.opt.runtimepath:prepend(lazypath)
-require("lazy").setup "plugins"
+require("lazy").setup("plugins", {
+	performance = {
+		rtp = {
+			disable_plugins = {
+				"netrw",
+				"netrwPlugin",
+				"netrwSettings",
+				"netrwFileHandlers",
+				"gzip",
+				"zip",
+				"zipPlugin",
+				"tar",
+				"tarPlugin",
+				"getscript",
+				"getscriptPlugin",
+				"vimball",
+				"vimballPlugin",
+				"2html_plugin",
+				"logipat",
+				"rrhelper",
+				"spellfile_plugin",
+			},
+		},
+	},
+})
 
 require "core.keymap"
