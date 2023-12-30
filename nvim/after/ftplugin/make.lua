a6c5c12dc0fd8566496326ea24dddaf880d06766
Liquid-system <80927666+Liquid-system@users.noreply.github.com>
2023-12-31 02:41:43 +0900

    indent

--- nvim/after/ftplugin/make.lua
+++ nvim/after/ftplugin/make.lua
@@ -1,4 +1,5 @@
 vim.opt_local.expandtab = false
-vim.opt_local.shiftwidth = 4
-vim.opt_local.softtabstop = 4
-vim.opt_local.tabstop = 4
+vim.opt_local.smarttab = false
+vim.opt_local.shiftwidth = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.tabstop = 2
