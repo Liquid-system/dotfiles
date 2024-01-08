d71cfbbe3b36a51876d985580eed4f939c896832
Liquid-system <80927666+Liquid-system@users.noreply.github.com>
2024-01-09 03:42:52 +0900

    fix

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -37,6 +37,8 @@ return {
         }),
       },
       mapping = {
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.abort(),
@@ -44,7 +46,7 @@ return {
           behavior = cmp.ConfirmBehavior.Replace,
           select = false,
         }),
-        ["<C-j>"] = cmp.mapping(function(fallback)
+        ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
           elseif luasnip.expand_or_jumpable() then
@@ -53,7 +55,7 @@ return {
             fallback()
           end
         end, { "i", "s" }),
-        ["<C-k>"] = cmp.mapping(function(fallback)
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
           elseif luasnip.jumpable(-1) then
