a35b2686f653a77e1aa6ec7de8d146734526d578
Liquid-system <80927666+Liquid-system@users.noreply.github.com>
2024-08-03 13:16:02 +0900

    Signed commit

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,21 +1,22 @@
+local colors = {
+  bg = "#202328",
+  fg = "#bbc2cf",
+  yellow = "#ECBE7B",
+  cyan = "#008080",
+  darkblue = "#081633",
+  green = "#98be65",
+  orange = "#FF8800",
+  violet = "#a9a1e1",
+  magenta = "#c678dd",
+  blue = "#51afef",
+  red = "#ec5f67",
+}
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     local lualine = require("lualine")
-    local colors = {
-      bg = "#202328",
-      fg = "#bbc2cf",
-      yellow = "#ECBE7B",
-      cyan = "#008080",
-      darkblue = "#081633",
-      green = "#98be65",
-      orange = "#FF8800",
-      violet = "#a9a1e1",
-      magenta = "#c678dd",
-      blue = "#51afef",
-      red = "#ec5f67",
-    }
+
 
     local conditions = {
       buffer_not_empty = function()
@@ -30,8 +31,6 @@ return {
         return gitdir and #gitdir > 0 and #gitdir < #filepath
       end,
     }
-
-    -- Config
     local config = {
       options = {
         -- Disable sections and component separators
