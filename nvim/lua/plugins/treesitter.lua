db0c0efa37575f0ad8bf2f95b59b27a698adb3ea
Liquid-system <80927666+Liquid-system@users.noreply.github.com>
2025-08-08 22:13:32 +0900

    treesitter master->main

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,9 @@
-if vim.g.treesitter_branch ~= 'main' then return {} end
-
--- on main branch, treesitter isn't started automatically
-vim.api.nvim_create_autocmd({ 'Filetype' }, {
+vim.api.nvim_create_autocmd({ "Filetype" }, {
   callback = function(event)
     -- make sure nvim-treesitter is loaded
     local ok, nvim_treesitter = pcall(require, 'nvim-treesitter')
-
     -- no nvim-treesitter, maybe fresh install
     if not ok then return end
-
     local ft = vim.bo[event.buf].ft
     local lang = vim.treesitter.language.get_lang(ft)
     nvim_treesitter.install({ lang }):await(function(err)
@@ -16,7 +11,6 @@ vim.api.nvim_create_autocmd({ 'Filetype' }, {
         vim.notify('Treesitter install error for ft: ' .. ft .. ' err: ' .. err)
         return
       end
-
       pcall(vim.treesitter.start, event.buf)
       vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
       vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
@@ -28,7 +22,4 @@ return {
   build = ":TSUpdate",
   branch = "main",
   lazy = false,
-  config = function()
-    require("nvim-treesitter").install({ "rust", "javascript", "zig", "python" })
-  end,
 }
